e46bbd3be03eb28dc82d19275225cb1e55e5cfdb
Blue ou <blue@Blue-3.local>
2019-05-12 16:34:13 -0700

    modify user util and actions and reducer

--- frontend/src/reducers/user_reducer.js
+++ frontend/src/reducers/user_reducer.js
@@ -1,12 +1,27 @@
-import { RECEIVE_CURRENT_USER } from "../actions/user_actions";
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT_CURRENT_USER
+} from "../actions/user_actions";
 
-const userReducer = (state = {}, action) => {
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+const userReducer = (state = initialState, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return Object.assign({}, state, {
-        [action.user.id]: action.user
-      });
+      return {
+        ...state,
+        isAuthenticated: !!action.user,
+        user: action.user
+      };
+    case LOGOUT_CURRENT_USER:
+      return {
+        isAuthenticated: false,
+        user: undefined
+      };
     default:
       return state;
   }
