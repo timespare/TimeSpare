aa5d4afea4d2d482eed14072a86698b60c174755
mengna <linmengna9392@gmail.com>
2019-05-13 11:08:23 -0700

    fixing gitignore

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://user:RvugOXz7SURKADau@timespare-gjm3e.mongodb.net/test?retryWrites=true",
+    "mongodb+srv://admin:0rlhssKqyqIMPzY5@mern-2jvs9.mongodb.net/test?retryWrites=true",
   secretOrKey: "secret"
 };
