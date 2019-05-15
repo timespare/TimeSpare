3d8e07c181c46705c58d0146fc3b74f49f8ba628
jinhuachen1314 <jic212@ucsd.edu>
2019-05-14 20:15:20 -0700

    fix ListingIndexItem conflict

--- frontend/src/components/listings/listing_index.jsx
+++ frontend/src/components/listings/listing_index.jsx
@@ -14,14 +14,7 @@ class ListingIndex extends React.Component {
     let listings = this.props.listings.map(listing => {
       return (
         <div className="listing-index-box">
-<<<<<<< HEAD
           <ListingIndexItem key={listing._id} listing={listing} isHome={this.props.isHome} />
-=======
-          <ListingIndexItem key={listing._id} listing={listing} isHome={this.props.isHome}/>
->>>>>>> 5e33f8c328f231bf8f964c9e3b8235583660b183
-          {/* <div className="index-button">
-            {this.renderButton(this.props.isHome, listing)}
-          </div> */}
         </div>
       );
     });
