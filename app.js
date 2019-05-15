7546be8f45a21fedc87ff653d10c05480d74f333
mengna <linmengna9392@gmail.com>
2019-05-15 10:29:29 -0700

    prepare for heroku

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const db = require("./config/keys").mongoURI;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const listings = require("./routes/api/listings");
 const bookings = require("./routes/api/bookings");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://admin:0rlhssKqyqIMPzY5@mern-2jvs9.mongodb.net/test?retryWrites=true",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
