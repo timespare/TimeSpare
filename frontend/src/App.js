5e4f7909820ad8f72b3ad3613d4bcfd7c82ec1e1
mattjang96 <mattjang95@gmail.com>
2019-06-06 15:21:58 -0700

    work on footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import UserProfile from "./components/UserProfile";
 import Splash from "./components/splash/Splash";
+import Footer from "./components/footer/Footer"
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Splash} />
+      <Route exact path="/" component={Footer} />
       <Route exact path="/profile" component={UserProfile} />
       <Route exact path="/home" component={Home} />
       {/* <Route path="/current" component={} /> */}
