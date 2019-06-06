cb7376b3880497a49f640b8c75583eda063bced4
mattjang96 <mattjang95@gmail.com>
2019-06-06 11:14:19 -0700

    / page is now splash page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import UserProfile from "./components/UserProfile";
-// import Splash from "./component/Splash";
+import Splash from "./components/splash/Splash";
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/profile" component={UserProfile} />
-      <Route exact path="/" component={Home} />
-      {/* <Route exact path="/" component={Splash} */}
+      {/* <Route exact path="/" component={Home} /> */}
+      <Route exact path="/" component={Splash} />
       {/* <Route path="/current" component={} /> */}
     </div>
   );
--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -1,1 +1,93 @@
 import React from 'react'
+import NavBar from "../NavBar";
+import NavBarButton from "../NavBarButton";
+import Modal from "../Modal";
+import SignupFormContainer from "../session_form/signup_form_container";
+import LoginFormContainer from "../session_form/login_form_container";
+import { connect } from "react-redux";
+import { logout } from "../../actions/user_actions";
+
+const mapStateToProps = state => ({
+    currentUser: state.session.user
+});
+const mapDispatchToProps = dispatch => ({
+    logout: () => dispatch(logout())
+});
+
+class Splash extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalisOpen: false,
+            formType: ""
+        };
+    }
+    render() {
+        const onClose = () => {
+            this.setState({ modalisOpen: false, formType: "" });
+        };
+
+        const onSwitch = formType => {
+            this.setState({ formType: formType });
+        };
+        const currentUser = this.props.currentUser;
+        return (
+            <div className="home-container">
+                <div className="navbar-container">
+                    <NavBar>
+                        {!!currentUser || (
+                            <NavBarButton
+                                label="Sign In"
+                                noBackground="true"
+                                onClick={() =>
+                                    this.setState({ modalisOpen: true, formType: "Sign In" })
+                                }
+                            />
+                        )}
+                        {!!currentUser || (
+                            <NavBarButton
+                                label="Sign Up"
+                                onClick={() =>
+                                    this.setState({ modalisOpen: true, formType: "Sign Up" })
+                                }
+                            />
+                        )}
+                        {currentUser && <NavBarButton label="My Profile" link="/profile" />}
+                        {currentUser && (
+                            <NavBarButton
+                                label="Log Out"
+                                onClick={this.props.logout}
+                                link="/"
+                                noBackground={true}
+                            />
+                        )}
+                    </NavBar>
+                </div>
+
+                <Modal
+                    open={this.state.modalisOpen}
+                    formType={this.state.formType}
+                    onClose={onClose}
+                >
+                    {this.state.formType === "Sign In" && (
+                        <LoginFormContainer
+                            onSwitchButtonClick={onSwitch}
+                            onClose={onClose}
+                        />
+                    )}
+                    {this.state.formType === "Sign Up" && (
+                        <SignupFormContainer
+                            onSwitchButtonClick={onSwitch}
+                            onClose={onClose}
+                        />
+                    )}
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Splash);
