211aad6f4b6e492109c363f10be6fdbc8f4af825
Yinglan Ou <yinglanou19@gmail.com>
2019-06-06 16:17:06 -0700

    add user rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import UserProfile from "./components/UserProfile";
+import DisplayUser from "./components/DisplayUser";
 function App() {
   return (
     <div className="App">
       <Route exact path="/profile" component={UserProfile} />
       <Route exact path="/" component={Home} />
+      <Route exact path="/users/:userId" component={DisplayUser} />
       {/* <Route path="/current" component={} /> */}
     </div>
   );
