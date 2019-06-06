09f7c53939811df5c8386d8165beef519c7720f9
mattjang96 <mattjang95@gmail.com>
2019-06-06 15:43:30 -0700

    finish splash page, with footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,8 +3,10 @@ import "./Footer.css"
 
 export default () => (
     <footer className="footer">
-        <a href="https://www.linkedin.com/in/matthew-jang-91936b130/"><i class="fab fa-linkedin"></i><h1>LinkedIn</h1></a>
-        <a href="https://github.com/mattjang96"><i class="fab fa-github"></i><h1>Github</h1></a>
-        <a href="https://mattjang96.github.io/Portfolio-Site/"><i className="fas fa-portrait"></i><h1>Portfolio</h1></a>
+        <a href="https://www.linkedin.com/in/yinglan-ou-750648187/"><i class="fab fa-linkedin"></i><h1>Yinglan Ou</h1></a>
+        <a href="https://www.linkedin.com/in/matthew-jang-91936b130/"><i class="fab fa-github"></i><h1>Matthew Jang</h1></a>
+        <a href="https://github.com/yinglanou19/TimeSpare"><i class="fab fa-github"></i><h1>GitHub</h1></a>
+        <a href="https://www.linkedin.com/in/mengna-lin/"><i className="fas fa-portrait"></i><h1>Mengna Lin</h1></a>
+        <a href="https://www.linkedin.com/in/jin-c-15167b187/"><i className="fas fa-portrait"></i><h1>Jinhua Chen</h1></a>
     </footer>
 )
