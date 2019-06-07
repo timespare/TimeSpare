23418d76d38fcb428face4f880e3ee98c7077ce7
Yinglan Ou <yinglanou19@gmail.com>
2019-06-06 19:03:25 -0700

    save merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,16 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import UserProfile from "./components/UserProfile";
-<<<<<<< HEAD
-import Splash from "./components/splash/Splash";
-import Footer from "./components/footer/Footer"
-
-=======
 import DisplayUser from "./components/DisplayUser";
 import Splash from "./components/splash/splash";
 import Footer from "./components/footer/Footer";
->>>>>>> userRating
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Splash} />
       <Route exact path="/" component={Footer} />
       <Route exact path="/profile" component={UserProfile} />
-<<<<<<< HEAD
-=======
       <Route exact path="/users/:userId" component={DisplayUser} />
->>>>>>> userRating
       <Route exact path="/home" component={Home} />
       {/* <Route path="/current" component={} /> */}
     </div>
