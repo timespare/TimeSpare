48f493a8dce64cf6aebedd65177f0ed0914b5cec
mengna <linmengna9392@gmail.com>
2019-06-11 13:41:43 -0700

    styling

--- frontend/src/components/listings/listing_index.jsx
+++ frontend/src/components/listings/listing_index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ListingIndexItem from "./listing_index_item";
-import { fetchListingsSearchResult } from "../../util/listing_api_util";
 import SearchBar from "../searchBar";
-import { receiveListingErrors } from "../../actions/listing_actions";
 class ListingIndex extends React.Component {
   constructor(props) {
     super(props);
@@ -36,33 +34,77 @@ class ListingIndex extends React.Component {
       this.props.getSearchedListings(this.state.keyword);
     }
   }
+
   render() {
-    let listings = this.props.listings.map((listing, i) => {
+    const gridContainer = {
+      marginLeft: "auto",
+      marginRight: "auto",
+      display: "grid",
+      gridColumnGap: "60px",
+      gridTemplateColumns: "400px 400px 400px",
+      // backgroundColor: "#2196F3",
+      padding: "10px"
+    };
+
+    const gridItem = {
+      // backgroundColor: "rgba(255, 255, 255, 0.8)",
+      // border: "1px solid rgba(0, 0, 0, 0.8)",
+      padding: "20px",
+      // fontSize: "30px",
+      display: "flex",
+      justifyContent: "center"
+
+      // textAlign: "center"
+    };
+    if (!this.props.listings && this.props.isHome) {
       return (
-        <div className="listing-index-box" key={i}>
-          <ListingIndexItem
-            listing={listing}
-            isHome={this.props.isHome}
-            deleteListing={this.props.deleteListing}
-            createBooking={this.props.createBooking}
-          />
-        </div>
+        <SearchBar
+          onChange={this.handleInput("keyword")}
+          onSubmit={this.handleSubmit}
+          keyword={this.state.keyword}
+        />
       );
-    });
-    if (this.props.isHome) {
-      return (
-        <div className="listing-index-container">
-          <SearchBar
-            onChange={this.handleInput("keyword")}
-            onSubmit={this.handleSubmit}
-            keyword={this.state.keyword}
-          />
-          {listings}
-        </div>
-      );
-    } else {
-      return <div className="listing-index-container">{listings}</div>;
+    } else if (!this.props.listings && !this.props.isHome) {
+      return <div />;
     }
+    return (
+      <>
+        <SearchBar
+          onChange={this.handleInput("keyword")}
+          onSubmit={this.handleSubmit}
+          keyword={this.state.keyword}
+        />
+        <div style={gridContainer}>
+          {this.props.listings.map((listing, i) => {
+            return (
+              // <div className="listing-index-box" key={i}>
+              <div style={gridItem}>
+                <ListingIndexItem
+                  listing={listing}
+                  isHome={this.props.isHome}
+                  deleteListing={this.props.deleteListing}
+                  createBooking={this.props.createBooking}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+    // if (this.props.isHome) {
+    //   return (
+    //     <div className="listing-index-container">
+    //       <SearchBar
+    //         onChange={this.handleInput("keyword")}
+    //         onSubmit={this.handleSubmit}
+    //         keyword={this.state.keyword}
+    //       />
+    //       {listings}
+    //     </div>
+    //   );
+    // } else {
+    //   return <div className="listing-index-container">{listings}</div>;
+    // }
   }
 }
 export default ListingIndex;
